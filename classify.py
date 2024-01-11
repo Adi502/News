23f0adbe29cb551d2fffce4d6f1bbbd4c51101d4
Adi502 <92010701+Adi502@users.noreply.github.com>
2024-01-11 20:30:23 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -17,8 +17,16 @@ def classify_new_article(article_url):
 
     # Make predictions using the trained model
     prediction = classifier.predict(text_tfidf)[0]
+    section_mapping = {
+    1: 'Bollywood',
+    2: 'Cities',
+    3: 'Education',
+    4: 'Political',
+    5: 'Sports'
+    }
+    section_name = section_mapping[prediction]
 
-    print(f'Predicted Section for the new article: {prediction}')
+    print(f'Predicted Section for the new article: {section_name}')
 
 if __name__ == "__main__":
     new_article_url = 'https://indianexpress.com/article/sports/cricket/india-versus-australia-low-key-early-audition-for-next-years-t20-world-cup-9038353/'
